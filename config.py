6cb0c41aa05727e627c587557fca1d2ef856a4a3
somoire <rayr30667@gmail.com>
2019-04-14 14:50:23 -0400

    almost done with the manage file

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+
+class Config:
+    """
+    General configuration parent class
+    """
+    NEWS_API_BASE_URL = "https://newsapi.org/v2/{}?apiKey={}"
+    ARTICLE_API_BASE_URL= "https://newsapi.org/v2/everything?sources={}&apiKey={}"
+    NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+    Config: The parent configuration class with General configuration settings
+    """
+    pass
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    Args:
+    Config:The parernt configuration class with General configuration settings
+    """
+    DEBUG = True
+
+config_options={
+'development': DevConfig,
+'production':ProdConfig
+}
