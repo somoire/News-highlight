885c1c4db9be43a4200bb91a0166f5a5fc9b3e7e
somoire <rayr30667@gmail.com>
2019-04-14 15:08:56 -0400

    The test is set to go

--- manage.py
+++ manage.py
@@ -1,20 +1,21 @@
+
 from app import create_app
 from flask_script import Manager,Server
 
-#Creating app instance
-app=create_app('development')
+# creating the app instance
+
+app = create_app('development')
 
 manager = Manager(app)
-
 manager.add_command('server',Server)
+
 @manager.command
 def test():
-    """
-    Running the unit tests
-    """
+    '''running unit tests'''
     import unittest
-    tests=unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     manager.run()
