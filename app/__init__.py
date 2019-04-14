2eee4e3dbc386a939e01d0d4aaa2e0ec13cb1311
somoire <rayr30667@gmail.com>
2019-04-14 11:08:21 -0400

    The config has been initialized

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,28 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
 
 
 # Initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+
+    #Creating the application configurations
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+
+    #Registering the blueprint
+    from.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    #setting config
+    from.request import configure_request
+    configure_request(app)
+
+    #Will add the views and forms
+    return app
