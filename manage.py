564f42e2b417a5bbae9d44609005baf08d6f94f8
somoire <rayr30667@gmail.com>
2019-04-14 15:09:41 -0400

    Nothing much to commit

--- manage.py
+++ manage.py
@@ -1,21 +1,20 @@
-
 from app import create_app
 from flask_script import Manager,Server
 
-# creating the app instance
-
-app = create_app('development')
+#Creating app instance
+app=create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
 
+manager.add_command('server',Server)
 @manager.command
 def test():
-    '''running unit tests'''
+    """
+    Running the unit tests
+    """
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests=unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-
-if __name__ == '__main__':
+if __name__=='__main__':
     manager.run()
