f5fa60f4676415b367753d652b29bed73146e1e9
somoire <rayr30667@gmail.com>
2019-04-16 04:36:11 -0400

    Imported the wtf forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,3 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,TextAreaField,SubmitField
-# from wtforms.validators import Required
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
